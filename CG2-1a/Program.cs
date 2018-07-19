25108cbfdee649a0aca9269320c88790768406ce
JaneWB <brownjannick@att.net>
2018-07-18 20:00:24 -0500

    Add project files.

---
+++ CG2-1a/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG2_1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
