d40c00695abac20380d53762a9848d570ed0295d
JaneWB <brownjannick@att.net>
2018-07-18 20:03:45 -0500

    Hello World modify

--- CG2-1a/Program.cs
+++ CG2-1a/Program.cs
@@ -6,7 +6,15 @@ namespace CG2_1a
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is you name?");
+            Console.WriteLine("Type your first name");
+            string myFirstName = Console.ReadLine();
+
+            Console.WriteLine("Type your last name");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine();
         }
     }
 }
